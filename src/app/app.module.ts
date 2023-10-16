3c34b08b685c7dfa541df18ef4628802e778af0d
hslape <awinter71417@gmail.com>
2023-10-15 21:10:06 -0500

    setting up & loading routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './users/user/user.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ServersComponent
+    ServersComponent,
+    UsersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
